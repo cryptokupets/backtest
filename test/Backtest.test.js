3ff7bd272865c9dc55bb117fbc0c9be5ac75fff4
Dmitry Fokin <fokind1@yandex.ru>
2020-10-30 10:09:00 +0300

    stoploss test

--- test/Backtest.test.js
+++ test/Backtest.test.js
@@ -39,4 +39,49 @@ describe("Backtest", () => {
             done();
         });
     });
+
+    it("stoploss", function () {
+        const candles = JSON.parse(fs.readFileSync("./test/data/data.json"));
+        const execute = (data) => {
+            return data[0].indicator("macd")[0] > 0 ? "buy" : "sell";
+        };
+        const strategy = new Strategy({
+            warmup: 1,
+            execute,
+            indicatorInputs: [
+                {
+                    key: "macd",
+                    name: "macd",
+                    options: [8, 17, 2],
+                },
+            ],
+        });
+
+        const options0 = {
+            candles,
+            strategy,
+            initialBalance: 100,
+            stoplossLevel: 0.99,
+            fee: 0,
+        };
+
+        const backtest0 = new Backtest(options0);
+
+        const options1 = {
+            candles,
+            strategy,
+            initialBalance: 100,
+            stoplossLevel: 0.95,
+            fee: 0,
+        };
+
+        const backtest1 = new Backtest(options1);
+
+        Promise.all([backtest0.execute(), backtest1.execute()]).then(() => {
+            console.log(backtest0.finalBalance);
+            console.log(backtest1.finalBalance);
+            assert.equal(backtest0.finalBalance, backtest1.finalBalance, "Stoploss не влияет на результат");
+            done();
+        });
+    });
 });
