344a9a31625bb7ad32db11d13bcb3f705dd34f26
Dmitry Fokin <fokind1@yandex.ru>
2020-11-06 16:26:58 +0300

    IndicatorService

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Backtest } from "./Backtest";
 import { ICandle } from "./ICandle";
 import { IdealBacktest } from "./IdealBacktest";
 import { IIndicatorInput } from "./IIndicatorInput";
+import { IndicatorService } from "./IndicatorService";
 import { IRoundtrip } from "./IRoundtrip";
 import { ITrade } from "./ITrade";
 import { Strategy } from "./Strategy";
@@ -11,6 +12,7 @@ export {
     Backtest,
     IdealBacktest,
     ICandle,
+    IndicatorService,
     IIndicatorInput,
     IRoundtrip,
     ITrade,
