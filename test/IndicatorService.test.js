671b6f0630a739dba1d4f6336c6d0645d30a1e54
Dmitry Fokin <fokind1@yandex.ru>
2020-11-30 15:53:07 +0300

    tect tulind

--- test/IndicatorService.test.js
+++ test/IndicatorService.test.js
@@ -5,19 +5,23 @@ const fs = require("fs");
 
 describe("IndicatorService", () => {
     it("execute", function (done) {
-        const candles = JSON.parse(fs.readFileSync("./test/data/data.json"));
+        const { candles, indicatorOutputs } = JSON.parse(fs.readFileSync("./test/data/data1.json"));
         const options = {
             candles,
-            name: "max",
-            options: [2],
+            name: "cci",
+            options: [5],
         };
 
         IndicatorService.execute(options).then((output) => {
-            // console.log(output);
+            console.log(output);
             assert.isArray(output, "массив данных");
             assert.isObject(output[0], "первый элемент является объектом");
             assert.isAtLeast(output.length, 1, "длина больше 1");
 
+            for (let index = 0; index < output.length; index++) {
+                assert.equal(indicatorOutputs[index].time, output[index].time);
+                assert.equal(indicatorOutputs[index].values[0], Math.round(output[index].values[0] * 100) / 100);
+            }
             done();
         });
     });
